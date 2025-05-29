a89d7bf6a6d8d0dec5ecb4f7c0e6db52e3f70cfc
ouyassine <oumaima.oys@gmail.com>
2025-05-29 20:01:34 +0100

    fix map

--- RMA_SIG/frontend/urls.py
+++ RMA_SIG/frontend/urls.py
@@ -2,7 +2,13 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
-    path('map/', views.map_view, name='map'),
+    path(
+        'map/',
+        login_required(views.map_view),           # or TemplateView.as_view(...) if it’s CBV
+        name='map'
+    ),
 ]
